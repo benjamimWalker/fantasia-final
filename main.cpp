7db5ac946507a77d2bd8426952e7cba12a7232ab
benjamimWalker <bhaskdelta2@gmail.com>
2021-07-12 23:34:10 -0300

    adicionado linha para iniciar allegro.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 
 int main()
 {
+    al_init();
     ALLEGRO_DISPLAY *tela;
 
     tela = al_create_display(720,480);
-    al_show_native_message_box(NULL, "Title", "Heading", "Text", "Button", ALLEGRO_MESSAGEBOX_ERROR);
+    al_show_native_message_box(NULL, "Title", "Heading", "Text", "Button", NULL);
+
     return 0;
 }
